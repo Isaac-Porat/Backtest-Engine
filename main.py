0803e5beee6d7a0e7e5f472a420236014f1875d4
ZakPorat <zakporat2023@gmail.com>
2024-03-01 15:42:13 -0800

    Fixed none type error

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ load_dotenv()
 ALPHA_VANTAGE_API_KEY = os.getenv('ALPHA_VANTAGE_API_KEY')
 
 class Algorithm():
-  def __init__(self):
+  def __init__(self, backtest):
+    self.backtest = backtest
     self.openValue = None
     self.highValue = None
     self.lowValue = None
@@ -15,7 +16,11 @@ class Algorithm():
   def runAlgorithm(self, data):
     print(data)
 
+    if all([self.closeValue is not None, self.openValue is not None, self.lowValue is not None, self.highValue is not None]):
+      if self.closeValue > self.openValue:
+        self.backtest.marketBuyOrder(0.25, 0.005)
+
 if __name__ == '__main__':
-  algorithm = Algorithm()
-  backtest = Backtest(ALPHA_VANTAGE_API_KEY, symbol='SPY', interval='5min')
+  backtest = Backtest(ALPHA_VANTAGE_API_KEY, symbol='TSLA', interval='5min', accountBalance=5000)
+  algorithm = Algorithm(backtest=backtest)
   backtest.runBacktest(algorithm=algorithm.runAlgorithm, startDate='2024-2-1', endDate='2024-2-27', afterHoursTrading=False)
